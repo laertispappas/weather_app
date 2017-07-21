6d3a9dd8360d1a93ac29e684672838092a90f6c0
Laertis Pappas <laertis.pappas@gmail.com>
2017-07-21 13:54:09 +0300

    Adds search city component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { MaterializeModule } from 'angular2-materialize';
@@ -7,15 +8,18 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SearchCityComponent } from './search-city/search-city.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SearchCityComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     MaterializeModule
   ],
---
+++ client/src/app/search-city/search-city.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-city',
+  templateUrl: './search-city.component.html',
+  styleUrls: ['./search-city.component.css']
+})
+export class SearchCityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fetchWeather() {
+
+  }
+
+}
