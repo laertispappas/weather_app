13c3be04a88ad5b7c00a29eb6922b545f1899c0a
Laertis Pappas <laertis.pappas@gmail.com>
2017-07-23 02:49:06 +0300

    Adds preloading when loading cities

--- client/src/app/cities-list/cities-list.component.ts
+++ client/src/app/cities-list/cities-list.component.ts
@@ -9,14 +9,18 @@ import {WeatherService} from '../services/weather.service';
 })
 export class CitiesListComponent implements OnInit {
   cities: Array<any>;
+  loading: boolean;
 
   constructor(private _weatherService: WeatherService) {
   }
 
   ngOnInit() {
+    this.loading = true;
     this._weatherService.getCities().subscribe((cities) => {
       this.cities = cities;
+      this.loading = false;
     }, (error) => {
+      this.loading = false;
       // TODO: handle error here with notification service
       console.log(error);
     })
